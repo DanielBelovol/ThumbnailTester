4bdfa44e9747406bcfac048cfde7de416652772b
Macbook <macbook@Macbooks-MacBook-Pro.local>
2025-04-13 09:08:53 +0300

    adeed some services and repositories

--- src/main/java/com/example/ThumbnailTester/repositories/ThumbnailRepository.java
+++ src/main/java/com/example/ThumbnailTester/repositories/ThumbnailRepository.java
@@ -1,10 +1,10 @@
 package com.example.ThumbnailTester.repositories;
 
-import com.example.ThumbnailTester.data.thumbnail.ThumbnailStats;
+import com.example.ThumbnailTester.data.thumbnail.ThumbnailData;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ThumbnailStatsRepository extends JpaRepository<ThumbnailStats,Long> {
+public interface ThumbnailRepository extends JpaRepository<ThumbnailData, Long> {
 
 }
--- src/main/java/com/example/ThumbnailTester/services/ThumbnailStatsService.java
+++ src/main/java/com/example/ThumbnailTester/services/ThumbnailStatsService.java
@@ -1,8 +1,19 @@
 package com.example.ThumbnailTester.services;
 
+import com.example.ThumbnailTester.data.thumbnail.ThumbnailStats;
+import com.example.ThumbnailTester.repositories.ThumbnailRepository;
+import com.example.ThumbnailTester.repositories.ThumbnailStatsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ThumbnailStatsService {
+    @Autowired
+    private ThumbnailStatsRepository statsRepository;
+    @Autowired
+    private ThumbnailRepository thumbnailRepository;
 
+    public ThumbnailStats getByThumbnailId(long thumbnailId){
+
+    }
 }
