ccee0f72e8b5043f72a65c7fed3da416eacdb772
DanielBelovol <danielbelovol08@gmail.com>
2025-04-21 21:29:27 +0300

    deleted ImageResult and edited TestService

--- src/main/java/com/example/ThumbnailTester/controller/WebSocketController.java
+++ src/main/java/com/example/ThumbnailTester/controller/WebSocketController.java
@@ -1,19 +1,29 @@
 package com.example.ThumbnailTester.controller;
 
 import com.example.ThumbnailTester.Request.ThumbnailRequest;
+import com.example.ThumbnailTester.services.ThumbnailTestService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
+import java.io.IOException;
+
 @Controller
 public class WebSocketController {
     private final SimpMessagingTemplate messagingTemplate;
+    @Autowired
+    private ThumbnailTestService thumbnailTestService;
     public WebSocketController(SimpMessagingTemplate messagingTemplate){
         this.messagingTemplate = messagingTemplate;
     }
-    @MessageMapping("/thumbnail/create")
+    @MessageMapping("/thumbnail/test")
     public void handleMessage(@Payload ThumbnailRequest request) {
-
+        try {
+            thumbnailTestService.runThumbnailTest(request);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
